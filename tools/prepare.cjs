a0ddb15ed71f0423678cc6989c86c722983b28b5
tuhacrt <a067800497@gmail.com>
2026-07-03 09:49:15 +0800

    chore: update deps

--- tools/prepare.cjs
+++ tools/prepare.cjs
@@ -1,6 +1,6 @@
 if (!process.env.CI) {
-  const { spawnSync } = require("child_process");
-  const r = spawnSync("lefthook", ["install"], { stdio: "inherit" });
+  const { spawnSync } = require('child_process')
+  const r = spawnSync('lefthook', ['install'], { stdio: 'inherit' })
 
-  process.exit(r.status ?? 0);
+  process.exit(r.status ?? 0)
 }
